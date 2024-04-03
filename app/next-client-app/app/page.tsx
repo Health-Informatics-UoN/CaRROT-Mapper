3c125658f5b5ac8e4a35a1c8ecbb3919b28397c5
Andy Rae <hello@rae.li>
2024-04-03 14:45:18 +0100

    Add homepage login

--- app/next-client-app/app/page.tsx
+++ app/next-client-app/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>Home</div>;
+import { LoginButton, LogoutButton } from "@/auth/components/user-auth";
+import { options } from "@/auth/options";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession(options);
+
+  const username = session?.token.user?.username;
+
+  return (
+    <div>
+      {session ? <LogoutButton /> : <LoginButton />}
+      User: {username}
+    </div>
+  );
 }
